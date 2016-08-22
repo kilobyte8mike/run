7a91c9cee332be829949f72de48a593bc7516df9
Mike Jewell <kilobyte8mike@gmail.com>
2016-08-22 16:34:20 -0400

    PORT

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ var express = require('express');
 var fakeArray = require ('./models/fakeArray.js');
 var app = express();
 
+var PORT = process.env.PORT || 3000;
+
  app.get('/', function (req, res){
   res.render('index.ejs', {
     dataArray: fakeArray
   });
  })
 
-app.listen(3000, function () {
+app.listen(PORT, function () {
   console.log('Example app listening on port 3000!');
 });
