c1684375eb7dfff6cc20edcfcb4f5f153c3649df
Mike Jewell <kilobyte8mike@gmail.com>
2016-08-23 13:20:42 -0400

    pre DB impl

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 var express = require('express');
-var fakeArray = require ('./models/fakeArray.js');
+var bodyParser  = require('body-parser');
 var app = express();
+var runController = require('./controllers/run.js');
+
 
 var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use('/run', runController);
+
+app.use(function( req, res, next){
+  console.log('use called');
+  next();
+});
+
+
+app.use(function( req, res, next){
+  console.log('use called again');
+  next();
+});
+
+
+app.use( '/mike' , function( req, res, next){
+  console.log('Mike called');
+  next();
+});
 
  app.get('/', function (req, res){
   res.render('index.ejs', {
